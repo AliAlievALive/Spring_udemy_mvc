e568fb859ceb3484e1448d0117f936ae0143c2d0
almiev <madarat@mail.ru>
2021-10-13 21:06:09 +0300

    Аннотация @RequestParam Lesson 60

--- src/main/java/com/zaurtregulov/spring/mvc/MyController.java
+++ src/main/java/com/zaurtregulov/spring/mvc/MyController.java
@@ -3,6 +3,7 @@ package com.zaurtregulov.spring.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -25,9 +26,8 @@ public class MyController {
 //    }
 
     @RequestMapping("/showDetails")
-    public String showEmpDetails(HttpServletRequest request, Model model) {
+    public String showEmpDetails(@RequestParam("employeeName") String empName, Model model) {
 
-        String empName = request.getParameter("employeeName");
         empName = "Mr. " + empName;
         model.addAttribute("nameAttribute", empName);
 
